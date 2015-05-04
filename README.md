9fdf1ff6bd7afb94741d1bc23afd0f5bbea8c4a9
Ian Serlin <ian@ianserlin.com>
2015-05-04 15:23:48 +0800

    ahh, fresh air

---
+++ README.md
@@ -0,0 +1,3 @@
+This package is not ready for you to use yet, 
+but suffice it to say I want form validation that
+doesn't screw with my html.
---
+++ package.js
@@ -0,0 +1,46 @@
+Package.describe({
+	name: 'useful:forms',
+	version: '0.0.1',
+	summary: 'Form Template Mixin',
+	git: 'https://github.com/usefulio/forms',
+	documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+
+	api.versionsFrom('1.1');
+
+	// ====== BUILT-IN PACKAGES =======
+
+	api.use([
+		'templating'
+		, 'underscore'
+	], ['client', 'server']);
+
+	api.use([
+		'jquery'
+		, 'reactive-dict'
+		, 'reactive-var'
+		, 'mongo'
+	], 'client');
+
+	// ====== 3RD PARTY PACKAGES =======
+
+	// ====== BOTH =======
+
+	// ====== SERVER =======
+
+	// ====== CLIENT =======
+
+	api.addFiles('forms.js', 'client');
+	api.addFiles('validations.js', 'client');
+	api.addFiles('regexps.js', 'client');
+
+	// ====== EXPORTS =======
+
+	api.export('Forms');
+});
+
+Package.onTest(function(api) {
+	api.use('tinytest');
+});
