c70ce0c6691b59daef2c51467f6956d0015e3a78
Joshua Ohlman <codewriterohs@gmail.com>
2017-02-15 09:16:02 -0800

    First pass at ci deployment

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'ohlman:forms',
-	version: '1.2.0',
+	version: '1.2.0-2',
 	summary: 'Fully reactive forms which don\'t mess with your html',
 	git: 'https://github.com/usefulio/forms',
 	documentation: 'README.md'
