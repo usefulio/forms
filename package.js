2169a427e19bbebcb3f0001af78efe4ef8c9c18f
Ian Serlin <ian@ianserlin.com>
2017-09-01 15:07:05 +0300

    allowing .forms-ignore class to exclude certain inputs from being watched

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	name: 'useful:forms',
-	version: '1.1.3',
+	version: '1.1.4',
 	summary: 'Fully reactive forms which don\'t mess with your html',
 	git: 'https://github.com/usefulio/forms',
 	documentation: 'README.md'
